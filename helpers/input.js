3778718e6c4f693b8eec52d98438cdef80fcc115
Logan Farci <farci.logan@gmail.com>
2021-12-01 20:33:49 +0100

    Day 1 completed ⭐⭐

---
+++ helpers/input.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const REQUIRE_ARGV_LENGTH = 3;
+
+const processLineByLine = async (inputFileName, lineReader, onDone) => {
+    const fileStream = fs.createReadStream(inputFileName);
+    const rl = readline.createInterface({input: fileStream, crlfDelay: Infinity});
+    for await (const line of rl) {
+      lineReader(line);
+    }
+    onDone();
+}
+
+const startDailyChallengeWith = (handler) => {
+    if (process.argv.length == REQUIRE_ARGV_LENGTH) {
+        handler(process.argv[2]);
+    } else {
+        console.error("Usage: ", process.argv[0], process.argv[1], "<inputFile>");
+    }
+};
+
+module.exports = {
+    processLineByLine: processLineByLine,
+    startDailyChallengeWith: startDailyChallengeWith
+};
