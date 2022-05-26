bb48c8f6e64bbaeafe63e902047586ba6b4b143f
Logan Farci <farci.logan@gmail.com>
2022-05-26 17:08:20 +0200

    The second part of the Dive! challenge is solved.

--- dotnet/AdventOfCode2021/Challenges/Challenges/DiveChallenge.cs
+++ dotnet/AdventOfCode2021/Challenges/Challenges/DiveChallenge.cs
@@ -5,12 +5,72 @@ namespace AdventOfCode2021.Challenges
     public class DiveChallenge : DailyChallenge
     {
 
-        public struct Coordinate
+        public interface ICoordinate
         {
-            private readonly int x;
+            public int X { get; }
+            public int Y { get; }
+            ICoordinate Apply(Instruction instruction);
+        }
+
+        public struct Instruction
+        {
+            private static int length = 2;
+            private static string[] validNames = {"forward", "down", "up"};
+
+            public string Name { get; }
+            public int Value { get; }
+
+            public static Instruction Parse(string text)
+            {
+                if (string.IsNullOrEmpty(text))
+                {
+                    throw new ArgumentException(nameof(text));
+                }
+
+                var coordinate = new Coordinate(0, 0);
+                string[] tokens = text.Split(' ');
+
+                if (tokens.Length != length)
+                {
+                    throw new ArgumentException(nameof(text));
+                }
+
+                try
+                {
+                    int value = Int32.Parse(tokens[1]);
+                    if (!validNames.Contains(tokens[0]))
+                    {
+                        throw new ArgumentException(nameof(text));
+                    }
+                    return new Instruction(tokens[0], value);
+                }
+                catch (FormatException e)
+                {
+                    throw new ArgumentException(nameof(text), e);
+                }
+            }
+
+            private Instruction(string name, int value)
+            {
+                Name = name;
+                Value = value;
+            }
+
+        }
+
+        public class Coordinate : ICoordinate
+        {
+            private static IDictionary<string, ICoordinate> directions = new Dictionary<string, ICoordinate>()
+            {
+                { "forward", new Coordinate(1, 0) },
+                { "down", new Coordinate(0, 1) },
+                { "up", new Coordinate(0, -1) },
+            };
+
+            protected readonly int x;
             public int X { get { return x; } }
 
-            private readonly int y;
+            protected readonly int y;
             public int Y { get { return y; } }
 
             public Coordinate(int x, int y)
@@ -19,20 +79,68 @@ namespace AdventOfCode2021.Challenges
                 this.y = y;
             }
 
-            public Coordinate Apply(Coordinate move)
+            public virtual ICoordinate Apply(Instruction instruction)
             {
-                return new Coordinate(x + move.x, y + move.y);
+                var direction = directions[instruction.Name];
+                ICoordinate move = new Coordinate(direction.X * instruction.Value, direction.Y * instruction.Value);
+                return Apply(move);
+            }
+
+            private ICoordinate Apply(ICoordinate move)
+            {
+                return new Coordinate(x + move.X, y + move.Y);
+            }
+
+            public override bool Equals(object? obj)
+            {
+                return obj is Coordinate coordinate &&
+                       x == coordinate.x &&
+                       y == coordinate.y;
+            }
+
+            public override int GetHashCode()
+            {
+                return HashCode.Combine(x, y);
             }
         }
 
-        private static int instructionLength = 2;
-
-        private static IDictionary<string, Coordinate> instructionsSet = new Dictionary<string, Coordinate>()
+        public class SubmarineCoordinate : Coordinate
         {
-            { "forward", new Coordinate(1, 0) },
-            { "down", new Coordinate(0, 1) },
-            { "up", new Coordinate(0, -1) },
-        };
+            private static IDictionary<string, SubmarineCoordinate> directions = new Dictionary<string, SubmarineCoordinate>()
+            {
+                { "forward", new SubmarineCoordinate(1, 0, 0) },
+                { "down", new SubmarineCoordinate(0, 0, 1) },
+                { "up", new SubmarineCoordinate(0, 0, -1) },
+            };
+
+            private readonly int aim;
+            public int Aim { get { return aim; } }
+
+            public SubmarineCoordinate(int x, int y, int aim) : base(x, y)
+            {
+                this.aim = aim;
+            }
+
+            public override ICoordinate Apply(Instruction instruction)
+            {
+                var direction = directions[instruction.Name];
+                return Apply(new SubmarineCoordinate(
+                    direction.X * instruction.Value,
+                    direction.Y * instruction.Value,
+                    direction.Aim * instruction.Value
+                ));
+            }
+
+            private SubmarineCoordinate Apply(SubmarineCoordinate move)
+            {
+                var depthMove = move.y;
+                if (move.x != 0)
+                {
+                    depthMove = aim * move.x;
+                }
+                return new SubmarineCoordinate(x + move.x, y + depthMove, aim + move.aim);
+            }
+        }
 
         private const string title = "Dive!";
         private const int day = 2;
@@ -41,59 +149,25 @@ namespace AdventOfCode2021.Challenges
         {
         }
 
-        public static Coordinate ParseInstructionFrom(string instruction)
+        public static ICoordinate FollowInstructionsFrom(ICoordinate coordinate, string[] lines)
         {
-            if (string.IsNullOrEmpty(instruction))
+            ICoordinate current = coordinate;
+            foreach (string line in lines)
             {
-                throw new ArgumentException(nameof(instruction));
-            }
-
-            var coordinate = new Coordinate(0, 0);
-            string[] tokens = instruction.Split(' ');
-
-            if (tokens.Length != instructionLength)
-            {
-                throw new ArgumentException(nameof(instruction));
-            }
-
-            try
-            {
-                int factor = Int32.Parse(tokens[1]);
-
-                if (!instructionsSet.ContainsKey(tokens[0]))
-                { 
-                    throw new ArgumentException(nameof(instruction));
-                }
-
-                Coordinate move = instructionsSet[tokens[0]];
-                coordinate = new Coordinate(move.X * factor, move.Y * factor);
-
-            }
-            catch (FormatException e)
-            {
-                throw new ArgumentException(nameof(instruction), e);
-            }
-
-            return coordinate;
-        }
-
-        public static Coordinate FollowInstructionsFrom(Coordinate coordinate, string[] instructions)
-        {
-            Coordinate current = coordinate;
-            foreach (string instruction in instructions)
-            {
-                Coordinate move = ParseInstructionFrom(instruction);
-                current = current.Apply(move);
+                Instruction instruction = Instruction.Parse(line);
+                current = current.Apply(instruction);
             }
             return current;
         }
 
         protected override void Run(string[] lines)
         {
-            Coordinate coordinate = new Coordinate(0, 0);
+            ICoordinate coordinate = new Coordinate(0, 0);
+            ICoordinate submarineCoordinate = new SubmarineCoordinate(0, 0, 0);
             coordinate = FollowInstructionsFrom(coordinate, lines);
-            var result = coordinate.X * coordinate.Y;
-            RegisterResultEntry($"puzzle answer is {result}");
+            submarineCoordinate = FollowInstructionsFrom(submarineCoordinate, lines);
+            RegisterResultEntry($"puzzle answer is {coordinate.X * coordinate.Y}");
+            RegisterResultEntry($"puzzle answer is {submarineCoordinate.X * submarineCoordinate.Y}");
         }
 
     }
--- dotnet/AdventOfCode2021/Tests/Challenges/DiveChallengeTests.cs
+++ dotnet/AdventOfCode2021/Tests/Challenges/DiveChallengeTests.cs
@@ -4,6 +4,94 @@ using System;
 
 namespace Tests.Challenges
 {
+
+    public class InstructionTests
+    {
+        [Fact]
+        public void Parse_EmptyString_ThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => DiveChallenge.Instruction.Parse(""));
+        }
+
+        [Fact]
+        public void Parse_InvalidInstruction_ThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => DiveChallenge.Instruction.Parse("3 back"));
+        }
+
+
+        [Fact]
+        public void Parse_UnknownInstruction_ThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => DiveChallenge.Instruction.Parse("unknown 3"));
+        }
+
+        [Fact]
+        public void Parse_SingleToken_ThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => DiveChallenge.Instruction.Parse("forward"));
+        }
+
+        [Fact]
+        public void Parse_ThreeTokens_ThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => DiveChallenge.Instruction.Parse("forward 2 fast"));
+        }
+
+        [Fact]
+        public void Parse_ForwardTwo_ReturnsExpectedInstruction()
+        {
+            var instruction = DiveChallenge.Instruction.Parse("forward 2");
+            Assert.Equal("forward", instruction.Name);
+            Assert.Equal(2, instruction.Value);
+        }
+
+        [Fact]
+        public void Parse_DownFive_ReturnsExpectedInstruction()
+        {
+            var instruction = DiveChallenge.Instruction.Parse("down 5");
+            Assert.Equal("down", instruction.Name);
+            Assert.Equal(5, instruction.Value);
+        }
+
+        [Fact]
+        public void Parse_UpFour_ReturnsExpectedInstruction()
+        {
+            var instruction = DiveChallenge.Instruction.Parse("up 4");
+            Assert.Equal("up", instruction.Name);
+            Assert.Equal(4, instruction.Value);
+        }
+    }
+
+    public class CoordinateTests
+    {
+        [Fact]
+        public void Apply_ForwardTwo_ReturnsCoordinateWithXIncrementedByTwo()
+        {
+            DiveChallenge.ICoordinate coordinate = new DiveChallenge.Coordinate(3, -1);
+            coordinate = coordinate.Apply(DiveChallenge.Instruction.Parse("forward 2"));
+            Assert.Equal(5, coordinate.X);
+        }
+
+        [Fact]
+        public void Apply_DownFour_ReturnsCoordinateWithYIncrementedByFour()
+        {
+            DiveChallenge.ICoordinate coordinate = new DiveChallenge.Coordinate(3, 100);
+            coordinate = coordinate.Apply(DiveChallenge.Instruction.Parse("down 4"));
+            Assert.Equal(104, coordinate.Y);
+        }
+
+
+        [Fact]
+        public void Apply_UpThree_ReturnsCoordinateWithYDecrementedByThree()
+        {
+            DiveChallenge.ICoordinate coordinate = new DiveChallenge.Coordinate(3, 100);
+            coordinate = coordinate.Apply(DiveChallenge.Instruction.Parse("up 3"));
+            Assert.Equal(97, coordinate.Y);
+        }
+
+    }
+
     public class DiveChallengeTests
     {
         private static string[] instructions = {
@@ -16,59 +104,7 @@ namespace Tests.Challenges
         };
 
         [Fact]
-        public void ParseInstructionFrom_EmptyString_ThrowsArgumentException()
-        {
-            Assert.Throws<ArgumentException>(() => DiveChallenge.ParseInstructionFrom(""));
-        }
-
-        [Fact]
-        public void ParseInstructionFrom_InvalidInstruction_ThrowsArgumentException()
-        {
-            Assert.Throws<ArgumentException>(() => DiveChallenge.ParseInstructionFrom("3 back"));
-        }
-
-
-        [Fact]
-        public void ParseInstructionFrom_UnknownInstruction_ThrowsArgumentException()
-        {
-            Assert.Throws<ArgumentException>(() => DiveChallenge.ParseInstructionFrom("unknown 3"));
-        }
-
-        [Fact]
-        public void ParseInstructionFrom_SingleToken_ThrowsArgumentException()
-        {
-            Assert.Throws<ArgumentException>(() => DiveChallenge.ParseInstructionFrom("forward"));
-        }
-
-        [Fact]
-        public void ParseInstructionFrom_ThreeTokens_ThrowsArgumentException()
-        {
-            Assert.Throws<ArgumentException>(() => DiveChallenge.ParseInstructionFrom("forward 2 fast"));
-        }
-
-        [Fact]
-        public void ParseInstructionFrom_ForwardTwo_ReturnsCoordinateWithXSetToTwo()
-        {
-            var coordinate = DiveChallenge.ParseInstructionFrom("forward 2");
-            Assert.Equal(2, coordinate.X);
-        }
-
-        [Fact]
-        public void ParseInstructionFrom_DownFive_ReturnsCoordinateWithYSetToFive()
-        {
-            var coordinate = DiveChallenge.ParseInstructionFrom("down 5");
-            Assert.Equal(5, coordinate.Y);
-        }
-
-        [Fact]
-        public void ParseInstructionFrom_UpFour_ReturnsCoordinateWithYSetToMinusFour()
-        {
-            var coordinate = DiveChallenge.ParseInstructionFrom("up 4");
-            Assert.Equal(-4, coordinate.Y);
-        }
-
-        [Fact]
-        public void ShouldBeExpectedXAfterFollowingInstructions()
+        public void FollowInstructionsFrom_CoordinateAndValidInstructions_RetunsCoordinateWithExpectedX()
         {
             var departure = new DiveChallenge.Coordinate(0, 0);
             var arrival = DiveChallenge.FollowInstructionsFrom(departure, instructions);
@@ -76,11 +112,39 @@ namespace Tests.Challenges
         }
 
         [Fact]
-        public void ShouldBeExpectedYAfterFollowingInstructions()
+        public void FollowInstructionsFrom_CoordinateAndValidInstructions_RetunsCoordinateWithExpectedY()
         {
             var departure = new DiveChallenge.Coordinate(0, 0);
             var arrival = DiveChallenge.FollowInstructionsFrom(departure, instructions);
             Assert.Equal(10, arrival.Y);
+        }
+
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(34, 2)]
+        [InlineData(-1, 34)]
+        [InlineData(34, -1)]
+        public void FollowInstructionsFrom_CoordinateAndNoInstructions_RetunsInputCoordinate(int x, int y)
+        {
+            var departure = new DiveChallenge.Coordinate(x, y);
+            var arrival = DiveChallenge.FollowInstructionsFrom(departure, new string[0]);
+            Assert.True(departure.Equals(arrival));
+        }
+
+        [Fact]
+        public void FollowInstructionsFrom_ValidInstructions_RetunsCoordinateWithExpectedX()
+        {
+            var departure = new DiveChallenge.SubmarineCoordinate(0, 0, 0);
+            var arrival = DiveChallenge.FollowInstructionsFrom(departure, instructions);
+            Assert.Equal(15, arrival.X);
+        }
+
+        [Fact]
+        public void FollowInstructionsFrom_ValidInstructions_RetunsCoordinateWithExpectedY()
+        {
+            var departure = new DiveChallenge.SubmarineCoordinate(0, 0, 0);
+            var arrival = DiveChallenge.FollowInstructionsFrom(departure, instructions);
+            Assert.Equal(60, arrival.Y);
         }
     }
 }
