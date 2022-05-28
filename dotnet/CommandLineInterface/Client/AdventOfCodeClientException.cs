c5744d0069205e6fd4608aa806de17d7c3c0ea41
Logan Farci <farci.logan@gmail.com>
2022-05-28 22:25:55 +0200

    Serializable attribute added to the AdventOfCodeClientException

--- dotnet/CommandLineInterface/Client/AdventOfCodeClientException.cs
+++ dotnet/CommandLineInterface/Client/AdventOfCodeClientException.cs
@@ -1,16 +1,19 @@
 ﻿namespace AdventOfCode2021.CommandLineInterface.Client
 {
+    [Serializable]
     public class AdventOfCodeClientException : Exception
     {
         public AdventOfCodeClientException()
         {
         }
 
-        public AdventOfCodeClientException(string? message) : base(message)
+        public AdventOfCodeClientException(string? message)
+            : base(message)
         {
         }
 
-        public AdventOfCodeClientException(string? message, Exception cause) : base(message, cause)
+        public AdventOfCodeClientException(string? message, Exception cause)
+            : base(message, cause)
         {
         }
     }
