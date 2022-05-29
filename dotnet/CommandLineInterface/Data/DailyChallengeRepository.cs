c0b0fd2195164fdf2a3d91ba676d17eea968580c
Logan Farci <farci.logan@gmail.com>
2022-05-29 10:28:14 +0200

    Syntax shorthand used to instantiate list in the DailyChallengeRepository

--- dotnet/CommandLineInterface/Data/DailyChallengeRepository.cs
+++ dotnet/CommandLineInterface/Data/DailyChallengeRepository.cs
@@ -4,7 +4,7 @@ namespace AdventOfCode2021.CommandLineInterface.Data
 {
     public static class DailyChallengeRepository
     {
-        private static List<DailyChallenge> challenges = new List<DailyChallenge>
+        private static List<DailyChallenge> challenges = new()
         {
             new SonarSweepChallenge(1, "Sonar Sweep"),
             new DiveChallenge(2, "Dive!"),
