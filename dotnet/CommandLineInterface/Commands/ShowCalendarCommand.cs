ea99632e590ced4115a681121aa388b1549a4f41
Logan Farci <farci.logan@gmail.com>
2022-05-30 22:03:37 +0200

    First version of the show calendar command.

---
+++ dotnet/CommandLineInterface/Commands/ShowCalendarCommand.cs
@@ -0,0 +1,87 @@
+﻿using AdventOfCode2021.CommandLineInterface.Client;
+using CommandLineInterface.Data;
+using Spectre.Console;
+using Spectre.Console.Cli;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineInterface.Commands
+{
+    public class ShowCalendarCommand : AsyncCommand<ShowCalendarCommand.Settings>
+    {
+        public class Settings : CommandSettings
+        {
+            [CommandArgument(0, "[year]")]
+            [Description("The year to show the Advent Of Code calendar for.")]
+            public string? Year { get; init; }
+        }
+
+        public override async Task<int> ExecuteAsync(CommandContext context, Settings settings)
+        {
+            try
+            {
+                int year = int.Parse(settings?.Year ?? "");
+                ICalendarRepository repository = CalendarRepository.Instance;
+                AdventOfCodeCalendar calendar = await repository.FindByYear(year);
+
+                var rule = new Rule($"Advent Of Code {year}");
+                AnsiConsole.Write(rule);
+
+                double veryComplete = 0.0;
+                double complete = 0.0;
+                double notStarted = 0.0;
+
+                foreach (var day in calendar.Days)
+                {
+                    AnsiConsole.MarkupLine($"Day {day.Index}: {day.Completion}");
+
+                    if (day.Completion == AdventOfCodeCalendar.Completion.Complete)
+                    {
+                        complete++;
+                    }
+
+                    if (day.Completion == AdventOfCodeCalendar.Completion.VeryComplete)
+                    {
+                        veryComplete++;
+                    }
+
+                    if (day.Completion == AdventOfCodeCalendar.Completion.NotStarted)
+                    {
+                        notStarted++;
+                    }
+
+                }
+
+                var count = calendar.Days.Count;
+
+                var chart = new BreakdownChart()
+                    .FullSize()
+                    .Width(50)
+                    .ShowPercentage()
+                    .AddItem("Very complete", Math.Round(veryComplete / count * 100, 2), Color.LightGreen)
+                    .AddItem("Complete", Math.Round(complete / count * 100, 2), Color.Green)
+                    .AddItem("To solve", Math.Round(notStarted / count * 100, 2), Color.Grey);
+
+                AnsiConsole.Write(new Panel(chart).Padding(1, 1).Header("Completion"));
+
+                AnsiConsole.Write(new Rule());
+
+                return 0;
+            }
+            catch (FormatException)
+            {
+                AnsiConsole.MarkupLine($"[red][bold]Invalid year:[/] {settings.Year}[/]");
+                return -1;
+            }
+            catch (AdventOfCodeClientException)
+            {
+                AnsiConsole.MarkupLine($"[red][bold]Failed to show calendar for the given year:[/] {settings.Year}[/]");
+                return -1;
+            }
+        }
+    }
+}
--- dotnet/CommandLineInterface/Program.cs
+++ dotnet/CommandLineInterface/Program.cs
@@ -1,58 +1,23 @@
 ﻿using AdventOfCode2021.Challenges;
 using AdventOfCode2021.CommandLineInterface.Data;
+using CommandLineInterface.Commands;
 using CommandLineInterface.Data;
+using Spectre.Console.Cli;
 
 namespace AdventOfCode2021
 {
     public static class Program
     {
-
-        private static readonly string ProgramFriendlyName = AppDomain.CurrentDomain.FriendlyName;
-        private static readonly IChallengeInputRepository inputRepository = ChallengeInputRepository.Instance;
-        private static readonly ICalendarRepository calendarRepository = CalendarRepository.Instance;
-
-        static async Task Main(string[] args)
+        public static int Main(string[] args)
         {
-            AdventOfCodeCalendar c = await calendarRepository.FindByYear(2020);
-
-            Console.WriteLine($"Calendar 2020:");
-
-            foreach (AdventOfCodeCalendar.Day day in c.Days)
+            var app = new CommandApp();
+            app.Configure(config =>
             {
-                Console.WriteLine($"\t- Index: {day.Index} Completion: {day.Completion}");
-            }
-
-            c = await calendarRepository.FindByYear(2021);
-
-            Console.WriteLine($"Calendar 2021:");
-
-            foreach (AdventOfCodeCalendar.Day day in c.Days)
-            {
-                Console.WriteLine($"\t- Index: {day.Index} Completion: {day.Completion}");
-            }
-
-            /*if (args.Length == 1)
-            {
-                DailyChallenge? challenge = DailyChallengeRepository.FindByDay(args[0]);
-                if (challenge != null)
-                {
-                    string[] lines = await inputRepository.FindInputLinesByYearAndDayAsync(DailyChallenge.Year, challenge.Day);
-                    IEnumerable<DailyChallenge.PuzzleAnswer> answers = challenge.Run(lines);
-                    Console.WriteLine($"Day {challenge.Day}: {challenge.Title}");
-                    foreach (DailyChallenge.PuzzleAnswer answer in answers)
-                    {
-                        Console.WriteLine($"\t- {answer.Description}: {answer.Value}.");
-                    }
-                }
-                else
-                {
-                    Console.WriteLine($"No daily challenge could be resolved with key: \"{args[0]}\".");
-                }
-            }
-            else
-            {
-                Console.WriteLine($"Usage: {ProgramFriendlyName} ## (Daily challenge key).");
-            }*/
+                config.AddCommand<ShowCalendarCommand>("calendar")
+                    .WithDescription("Show the Advent Of Code calendar for the given year.")
+                    .WithExample(new[] { "calendar", "2020" });
+            });
+            return app.Run(args);
         }
     }
 }
